d14f7f934f4ce0e2bf1a562062cf081f4ccb9097
Rishitkesharwani <rishitkesharwani@gmail.com>
2022-12-31 17:34:29 +0530

    Finding area of rectangle

---
+++ area_of_rectangle.c
@@ -0,0 +1,12 @@
+# include<stdio.h>
+float main()
+{
+float  lenght,bredth;
+printf("Enter the value of lenght and bredth \n");
+scanf("%f %f" ,&lenght,&bredth);
+printf("The area of rectangle is %f \n" , lenght*bredth);
+
+return 0;
+
+}
+
