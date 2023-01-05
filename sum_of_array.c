2263eaeeb9f9760d4c11b14edadf72daa0957f5c
Rishitkesharwani <rishitkesharwani@gmail.com>
2023-01-05 16:45:07 +0530

    sum of two array

---
+++ sum_of_array.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+ {
+int n,sum=0;
+    printf("Enter the size of array:");
+    scanf("%d",&n);
+    int a[n];
+    printf("Enter the element of array:");
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(int i=0;i<n;i++)
+    {
+        sum+=a[i];
+    }
+    printf("sum of Elemnet in array is:%d ",sum);
+    return 0;
+ }
