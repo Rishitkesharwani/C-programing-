7f6831c5599cbfd2e03071315f079ad4be0e38df
Rishitkesharwani <rishitkesharwani@gmail.com>
2023-01-09 15:49:40 +0530

    Finding square of number

---
+++ sqrt.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    float a,c;
+    printf("Enter the values: ");
+    scanf("%f",&a);
+    c=sqrt(a);
+    printf( "Square root of Enter value is: %f",c);
+    return 0;
+}
