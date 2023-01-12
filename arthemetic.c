9f03d5d9df267851b00447488305a7a8c3240572
Rishitkesharwani <rishitkesharwani@gmail.com>
2023-01-12 20:54:41 +0530

    Performing airthmathic function

---
+++ arthemetic.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+        int a,b;
+    printf("Enter the value of a and b: ");
+    scanf("%d %d" ,&a,&b);
+    printf("sum of numbers are %d\n",a+b);
+    printf("Substract of both number is %d\n",a-b);
+    printf("Mulitple of number is %d\n",a*b);
+    printf("Divide of numbers is %d\n",a%b);
+    return 0;
+}
