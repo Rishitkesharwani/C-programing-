cad41ba5e88ac99ef69247d847f5d2040b463586
Rishitkesharwani <rishitkesharwani@gmail.com>
2023-01-15 12:39:41 +0530

    Finding sum of two number

---
+++ sum.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b;
+    printf("Enter the value of a and b: ");
+    scanf("%d %d" ,&a,&b);
+    printf("sum of numbers are %d",a+b);
+    return 0;
+}
