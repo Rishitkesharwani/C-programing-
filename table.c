345d51ec3468151e0c15a2936cb1133d6620ee52
Rishitkesharwani <rishitkesharwani@gmail.com>
2023-01-15 12:41:12 +0530

    Printing table of a given number

---
+++ table.c
@@ -0,0 +1,38 @@
+                      //factorial of a number using for
+       // #include<stdio.h>
+
+// int main(){
+//     // factorial(4) - 1 * 2 * 3 * 4 
+//     // factorial(6) - 1 * 2 * 3 * 4 * 5 * 6
+//     int i=0, n, factorial=1;
+//     printf("Enter the whose factorial you want print:");
+//     scanf("%d",&n);
+//     for(i=1;i<=n;i++){
+//         factorial *=i;
+//     }
+//     printf("The value of factorial %d is %d", n, factorial);
+//     return 0;
+// }
+ 
+                              
+                            //Forctial of number using while loop
+
+    //  #include<stdio.h>
+     
+    //  int main()
+    //  {
+    //     int i=1,n,factorial=1;
+    
+    //  printf("Enter the number whose factorial you want print:");
+    //  scanf("%d",&n);
+    
+    //  while(i<=n)
+    //  {
+    //     factorial *=i;
+    //     i++;
+    //     }
+    //     printf("The value of factorial %d is %d", n, factorial);
+    //     return 0;
+    //  }
+
+                        
