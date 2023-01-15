eb0ca0646568bf935559a3e966dc64b4a1e7d846
Rishitkesharwani <rishitkesharwani@gmail.com>
2023-01-15 12:38:34 +0530

    Finding the simple intrest of a given number

---
+++ simple_intrest.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int p,n;
+    float r,si;
+    printf("Enter the of value  of p,n,r: ");
+    scanf("%d %d %f",&p,&n,&r);
+    si=p*r*n/100
+    printf("Simple intrest is: %f",si);
+    return 0;
+}
