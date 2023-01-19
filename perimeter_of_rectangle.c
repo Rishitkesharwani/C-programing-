0f6e84e16db18490fe8f72954a6b6d720c518efe
Rishitkesharwani <rishitkesharwani@gmail.com>
2023-01-19 12:11:41 +0530

    Finding the perimeter rectangle

---
+++ perimeter_of_rectangle.c
@@ -0,0 +1,12 @@
+# include<stdio.h>
+float  main()
+{
+float lenght,bredth;
+printf("Enter the value of lenght and bredth \n");
+scanf("%f %f" ,&lenght,&bredth);
+printf("The perimeter of rectangle is %f \n" , 2*(lenght+bredth));
+
+return 0;
+
+}
+
