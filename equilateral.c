0199a7a1e18858406a44f91306e21153ec631f67
Rishitkesharwani <rishitkesharwani@gmail.com>
2023-01-31 19:23:46 +0530

    Finding a Triangle is equilateral or not

---
+++ equilateral.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    float a,b;
+    printf("Enter the values: ");
+    scanf("%f",&a);
+    b=1.732*a*a/4;
+    printf( "Area of Equilateral trinagle is: %f",b);
+    return 0;
+}
