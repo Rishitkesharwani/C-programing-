35541089feaaf15f88d00c4e24beb489d3008702
Rishitkesharwani <rishitkesharwani@gmail.com>
2023-02-01 09:47:58 +0530

    Printing factorial of a number using for loop

---
+++ factoril_for_loop.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+ 
+ int main()
+ {
+  int n,factorial=1;
+printf("Enter the number whose factorial you want print:");
+scanf("%d",&n);
+for(int i=1;i<=n;i++)
+{
+  factorial *=i;
+}
+printf("The factoril of %d is %d",n,factorial);
+  return 0;
+ }
