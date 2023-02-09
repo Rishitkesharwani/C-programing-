402f79e7a19c33bcb864e1d15c8672d90d33a98e
Rishitkesharwani <rishitkesharwani@gmail.com>
2023-02-09 18:16:21 +0530

    Using Xor operator

---
+++ xor_operation.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+   int a,b,xor;
+   printf("Enter the value of a:");
+   scanf("%d",&a);
+   printf("Enter the value of b:");
+   scanf("%d",&b);
+   xor=a^b;
+printf("%d",xor);
+return 0;
+}
