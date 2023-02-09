952203aa4044d25fa983b61872b822b6484def11
Rishitkesharwani <rishitkesharwani@gmail.com>
2023-02-09 18:16:59 +0530

    Using input and output operator

---
+++ input_output.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    int a;
+    float b;
+    char c;
+    printf("Enter the value of int  float and char: ");
+    scanf("%d %f %s",&a,&b,&c);
+    printf("The value of the inputs in int float and char are:  %d %f  %c\n",a,b,c);
+    
+ return 0;
+}
