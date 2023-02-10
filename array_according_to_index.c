718deefe1da32c1099810e6256cf3823f3653378
Rishitkesharwani <rishitkesharwani@gmail.com>
2023-02-10 22:52:06 +0530

    Printing array according to index

---
+++ array_according_to_index.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int a[5]={10,20,30,40,50};
+    printf("Element in array is %d\n",a[0]);
+    printf("Element in array is %d\n",a[1]);
+    printf("Element in array is %d\n",a[2]);
+    printf("Element in array is %d\n",a[3]);
+    printf("Element in array is %d\n",a[4]);
+
+   
+return 0;
+}
