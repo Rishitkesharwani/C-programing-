da0f61a52c0adf92859295568c61da04404b07c2
Rishitkesharwani <rishitkesharwani@gmail.com>
2023-02-11 12:40:46 +0530

    Sparse matrix

---
+++ sparse_matrix.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b
+    printf("Enter the size of matrix:");
+    scanf("%d %d",&a,&b);
+    printf("Enter the Element of Matrix:");
+    for (int i=0;i<n;i++)
+    {
+        for(int j=o;j<m;j++)
+        {
+            scanf("%d",a[i][j])
+        }
+        printf("\n");
+    }
+    int count=0;
+    
+
+    return 0;
+}
